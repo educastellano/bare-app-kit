ca9a14e2e23c39e05d2e884c947977d6967f3ebc
Eduard Castellano <educastellano08@gmail.com>
2026-07-05 01:28:39 +0200

    Add `Control`

--- binding.m
+++ binding.m
@@ -6,6 +6,7 @@
 #import <AppKit/AppKit.h>
 
 #import "lib/button.h"
+#import "lib/control.h"
 #import "lib/window.h"
 
 static js_value_t *
@@ -24,6 +25,21 @@ bare_app_kit_exports(js_env_t *env, js_value_t *exports) {
   V("buttonInit", bare_app_kit_button_init)
   V("buttonGetTitle", bare_app_kit_button_get_title)
   V("buttonSetTitle", bare_app_kit_button_set_title)
+
+  V("controlEnabled", bare_app_kit_control_enabled)
+  V("controlContinuous", bare_app_kit_control_continuous)
+  V("controlIgnoresMultiClick", bare_app_kit_control_ignores_multi_click)
+  V("controlHighlighted", bare_app_kit_control_highlighted)
+  V("controlRefusesFirstResponder", bare_app_kit_control_refuses_first_responder)
+  V("controlTag", bare_app_kit_control_tag)
+  V("controlSize", bare_app_kit_control_size)
+  V("controlStringValue", bare_app_kit_control_string_value)
+  V("controlIntValue", bare_app_kit_control_int_value)
+  V("controlIntegerValue", bare_app_kit_control_integer_value)
+  V("controlFloatValue", bare_app_kit_control_float_value)
+  V("controlDoubleValue", bare_app_kit_control_double_value)
+  V("controlSizeToFit", bare_app_kit_control_size_to_fit)
+  V("controlPerformClick", bare_app_kit_control_perform_click)
 
   V("windowInit", bare_app_kit_window_init)
   V("windowContentView", bare_app_kit_window_content_view)
@@ -51,6 +67,11 @@ bare_app_kit_exports(js_env_t *env, js_value_t *exports) {
   V("WINDOW_STYLE_MASK_RESIZABLE", NSWindowStyleMaskResizable)
   V("WINDOW_STYLE_MASK_FULL_SCREEN", NSWindowStyleMaskFullScreen)
   V("WINDOW_STYLE_MASK_FULL_SIZE_CONTENT_VIEW", NSWindowStyleMaskFullSizeContentView)
+
+  V("CONTROL_SIZE_REGULAR", NSControlSizeRegular)
+  V("CONTROL_SIZE_SMALL", NSControlSizeSmall)
+  V("CONTROL_SIZE_MINI", NSControlSizeMini)
+  V("CONTROL_SIZE_LARGE", NSControlSizeLarge)
 #undef V
 
   return exports;
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 exports.Button = require('./lib/button')
+exports.Control = require('./lib/control')
 exports.Window = require('./lib/window')
